feed123ed8314eb64018692c4c98cb9db587bd5d
Sergi Triado <55805318+striado@users.noreply.github.com>
2023-03-14 01:14:29 +0100

    maps

--- control.js
+++ control.js
@@ -1,9 +1,14 @@
 "use strict";
 
+var gMaps;    // Mapa Google Maps
+
 function init(){
     $('#consulta').on('click', consulta);
     $('#dataFutura').hide();
     $('#simbols').hide();
+    $('#taulaDades').hide();
+    $('#mapa').hide();
+
 }
 
 function consulta(){
@@ -11,11 +16,11 @@ function consulta(){
     let municipi = $('#municipi').val();
     let tipus = $('#tipus').val();
     console.log(data, municipi, tipus);
-
+    // Si no se'ns retorna cap error enviem la petició
     if(!validacioDades(data, municipi)){
         
         let dades = {
-            "$select" : "data,municipi,tipus_estacio,area_urbana,contaminant,h01,unitats,latitud,longitud,nom_comarca,codi_ine",
+            "$select" : "*",
             "$limit" : 100,
             "$$app_token" : "x0fIzm8MqVpAawmYUPekjqYGG"
         };
@@ -35,41 +40,51 @@ function consulta(){
             type: "GET",
             data: dades
         }).done(function(data) {
-          console.log(data);
+          $('#mapa').show();
+          crearMapa(data);
+          $('#taulaDades').show();
           populateTaula(data);
         });
     }
 }
-            
+// Validem les dades dels inputs            
 function validacioDades(data, municipi){
     let dataActual = dataAvui();
-    let errors = false;
+    let errors = {
+        data : false,
+        municipi: false
+    };
 
     if (data) {   
         if (data > dataActual) {
             $('#data').addClass('is-invalid');
             $('#dataFutura').show();
-            errors = true;
+            errors.data = true;
         } else {
             $('#data').removeClass('is-invalid');
             $('#dataFutura').hide();
-            errors = false;
+            errors.data = false;
         }
     }
     if (municipi) {    
         if (/^[a-z ]+$/i.test(municipi)) {
             $('#municipi').removeClass('is-invalid');
             $('#simbols').hide();
-            errors = false;
+            errors.municipi = false;
         } else {
             $('#municipi').addClass('is-invalid');
             $('#simbols').show();
-            errors = true;
+            errors.municipi = true;
         }
     }
-    return errors;
+    // Si no hi han errors de cap tipus retornem false
+    if (!errors.data && !errors.municipi) {
+        return false;
+    } else {
+        return true;
+    }
 }
-
+// Funció que retorna la data d'avui en format yyyy-MM-dd per comprovar que la data de l'input no sigui futura
 function dataAvui() {
     var d = new Date(),
         month = '' + (d.getMonth() + 1),
@@ -85,26 +100,20 @@ function dataAvui() {
 }
 
 function populateTaula(dades){
-    $('#taulaDades').append(`
-        <table class="table"> 
-            <thead>
-                <tr>
-                    <th scope="col">Data</th>
-                    <th scope="col">Municipi</th>
-                    <th scope="col">Tipus d'estació</th>
-                    <th scope="col">Area Urbana</th>
-                    <th scope="col">Contaminant</th>
-                    <th scope="col">Nivell</th>
-                    <th scope="col">Nom de la Comarca</th>
-                    <th scope="col">Codi Postal</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    `);
+    // Buidem la taula per si de cas i la tornem a crear
+    $('tbody').empty();
 
     dades.forEach(fila => {
+        let nivell;
+        // Iterem cada resultat per buscar les propietats que comencem en 'h', ja que els nivells de contaminant es guarden segons la hora en la que es troben
+        for (const [clau, valor] of Object.entries(fila)) {
+            // Quan trobem una propietat que comenci amb h, assignem el valor i surtim, doncs no té sentit seguir iterant
+            if (/^h/i.test(clau)) {
+                nivell = valor;
+                break;
+            }
+        }
+        // Emplenem la fila, i si després de tota la iteració la variable nivell segueix buida, entenem que és un nivell desconegut o que directament no l'han guardat
         $('tbody').append(`
             <tr>
                 <td>${fila.data.split('T')[0]}</td>
@@ -112,10 +121,42 @@ function populateTaula(dades){
                 <td>${fila.tipus_estacio}</td>
                 <td>${fila.area_urbana}</td>
                 <td>${fila.contaminant}</td>
-                <td>${fila.h01} ${fila.unitats}</td>
+                <td>${nivell ?? 'Nivell desconegut'} ${fila.unitats}</td>
                 <td>${fila.nom_comarca}</td>
                 <td>${fila.codi_ine}</td>
             </tr>
         `);
+        // I marquem el punt al mapa segons es va emplenant la taula
+        marcarPunt(parseFloat(fila.latitud), parseFloat(fila.longitud), fila.nom_estacio, fila.contaminant, nivell, fila.unitats);
     });
+}
+
+
+// Crear mapa Google Maps
+function crearMapa() {
+    gMaps = new google.maps.Map(
+        document.getElementById('mapa'),    // Element on dibuixar el mapa
+        {
+            center: {lat: 41.82, lng: 1.7},    // Latitut i longitut del centre del mapa
+            zoom: 8                            // Ampliació
+        });
+}
+
+// Crear marcador de Google Maps
+function crearMarcador(latitut, longitut, text) {
+    return new google.maps.Marker(
+        {
+            position: {lat: latitut, lng: longitut},    // Latitut i longitut
+            title: text                                 // Text del "tooltip"
+        });
+}
+
+// Posar el marcador de l'institut en el mapa
+function marcarPunt(lat, long, nom_estacio, molecula, nivell, unitat) {
+
+    let gMark;
+    // Crear marcador
+    gMark = crearMarcador(lat, long, `${nom_estacio}\n${molecula} ${nivell} ${unitat}`);
+    // Situar-lo en el mapa
+    gMark.setMap(gMaps);
 }
